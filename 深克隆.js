ed5e57e2a5e4c1ebceecbbd737dbd6401c10db09
yinhongnian <yinhongnian@hdsc.com>
2018-03-30 16:12:56 +0800

    issueNum: Description: Author & Date:

---
+++ 深克隆.js
@@ -0,0 +1,68 @@
+// 对象类型的判断
+const isType = (obj, type) => {
+  if (typeof obj !== 'object') return false
+  const typeString = Object.prototype.toString.call(obj)
+  let flag
+  switch(type) {
+    case 'Array':
+      flag = typeString === '[Object Array]'
+      break
+    case 'Date':
+      flag = typeString === '[Object Date]'
+      break
+    case 'RegExp':
+      flag = typeString === '[Object RegExp]'
+      break
+    default:
+      flag = false
+  }
+  return flag
+}
+
+// 正则的提取flags的函数
+const getRegExp = re => {
+  var flags = '';
+  if(re.global) flags += 'g'
+  if(re.ignoreCase) flags += 'i'
+  if(re.multiline) flags += 'm'
+  return flags
+}
+
+// 深克隆
+const clone = parent => {
+  const parents = []
+  const children = []
+
+  const _clone = parent => {
+    if(parent === null) return null
+    if(typeOf(parent) !== 'object') return parent
+    let child, proto
+    if(isType(parent, 'Array')){
+      child = []
+    } else if(isType(parent, 'RegExp')){
+      child = new RegExg(parent.source, getRegExp(parent))
+      if(parent.lastIndex) child.lastIndex = parent.lastIndex
+    } else if(isType(parent, 'Date')){
+      // 对Date对象做特殊处理
+      child = new Date(parent.getTime())
+    } else {
+      // 处理对象原型
+      proto = Object.getPrototypeOf(parent)
+      // 利用Object.create切断原型链
+      child = Object.create(proto)
+    }
+
+    // 处理循环引用
+    const index = parents.indexOf(parent)
+    if(index !== -1){
+      return children[index]
+    }
+    parents.push(parent)
+    children.push(child)
+    for(let i in parent){
+      child[i] = _clone(parent[i])
+    }
+    return child
+  }
+  return _clone(parent)
+}
